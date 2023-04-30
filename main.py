6caacac31197ee1613527c5e77b173bd2926f885
Pelmeshek <131592010+Pelmeshechek@users.noreply.github.com>
2023-04-30 14:46:22 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from pygame import *
+from random import *
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, player_width, player_height):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (player_width,player_height))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+player = GameSprite('rocket.png', 335, 400, 6, 60, 85)
+
+ball = GameSprite('///', 10, 10, 4, 50, 50)
